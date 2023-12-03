00a1558a212bab9116cf4397dac59879f3522cfa
Josip Bošnjak <jbosnjak3@gmail.com>
2023-12-03 13:59:12 +0100

    added names of files for application settings, and log txt file name

--- GUI for CRUD operations for new type of gallery/src/entities/File.java
+++ GUI for CRUD operations for new type of gallery/src/entities/File.java
@@ -1,7 +1,12 @@
 package entities;
 
 public class File {
+//name of the file of database config	
 private final String fileName="databaseConfig.json";
+//this is the name of application setting file
+private final String appConfig="applicationConfig.json";
+//this is a name of file where log will be saved.
+private final String logFile="logFile.txt";
 
 
 public File() {
@@ -12,5 +17,12 @@ public String getFilename() {
 	return fileName;
 }
 
+public String getAppConfigFileName() {
+   return appConfig;	
+}
+
+public String getLogFile() {
+	return logFile;
+}
 
 }
