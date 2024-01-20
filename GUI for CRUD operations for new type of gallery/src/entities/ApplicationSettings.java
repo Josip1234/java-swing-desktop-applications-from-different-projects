602e1d1df7ee780848de28caffddfa50d9d61a5d
Josip Bošnjak <jbosnjak3@gmail.com>
2024-01-20 14:07:58 +0100

    added some comments there is some problem
    there is a problem with catching columns some extra tables just showed up we need to fix this

--- GUI for CRUD operations for new type of gallery/src/entities/ApplicationSettings.java
+++ GUI for CRUD operations for new type of gallery/src/entities/ApplicationSettings.java
@@ -1,15 +1,29 @@
 package entities;
 
+/***
+ * @author Josip Bošnjak
+ * @since 03.12.2023.
+ * @version 1.0
+ * <h1>Description</h1>
+ * <p>This is a entity for overriding default application settings. For example, you maybe want to display return message to the log.</p>
+ */
 public class ApplicationSettings {
+
 private String showMessages;
 
-//if you want to override setting, use this constructor
+/***
+ * 
+ * @param showMessages - default string which will be written in json shape in json file as default setting of displaying messages.
+ *<h1>if you want to override setting, use this constructor</h1>
+ */
 public ApplicationSettings(String showMessages) {
 	
 	this.showMessages = showMessages;
 }
 
-//default message show is in as popup messages
+/***
+ * <h1>Message logs will be displayed in a shape of popup window with okay button</h1>
+ */
 public ApplicationSettings() {
 	this.showMessages=GeneralMessages.popups;
 }
